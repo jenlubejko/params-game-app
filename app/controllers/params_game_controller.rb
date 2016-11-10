4dd69befdd7656c2b7830a666405a172f9a18a1b
Jennifer Lubejko <jennifer.lubejko@gmail.com>
2016-11-09 20:04:12 -0600

    Add game problem

--- app/controllers/params_game_controller.rb
+++ app/controllers/params_game_controller.rb
@@ -4,4 +4,16 @@ class ParamsGameController < ApplicationController
     @name = params['my_name']
     render 'query_params_game.html.erb'
   end
+  def guessing_game_method
+    winning_number = 32
+    @guess = params["the_guess"].to_i
+    if @guess > winning_number
+      @message = "Pick lower!"
+    elsif @guess < winning_number
+      @message = "Pick higher!"
+    else
+      @message = "You win!"
+    end
+    render 'guess.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/
   get '/query_params_game_url' => 'params_game#query_params_game_method'
+  get '/query_guess' => 'params_game#guessing_game_method'
 end
