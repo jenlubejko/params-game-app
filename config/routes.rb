a6f6c6b842dd551fcb7155d8562771be50102278
Jennifer Lubejko <jennifer.lubejko@gmail.com>
2016-11-10 18:48:58 -0600

    Fixing error in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'url_segment_params_url/:this_is_a_variable' => 'params_game#url_segment_params_method'
   get '/query_guess' => 'params_game#guessing_game_method'
   get '/form_params_url' => 'params_game#form_params_method'
-  post '/form_result_url' => 'params_examples#form_result_method'
+  post '/form_result_url' => 'params_game#form_result_method'
 end
